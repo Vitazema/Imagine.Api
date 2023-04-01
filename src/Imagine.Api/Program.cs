f3e2f753a59a1ab3e6e5e3225bf135a2a6f19a04
Vitalii Malozemov <v.malozyomov@itransition.com>
2023-04-01 11:25:48 +0300

    Fix local development; Fix db DateTime

--- src/Imagine.Api/Program.cs
+++ src/Imagine.Api/Program.cs
@@ -4,7 +4,11 @@ using Imagine.Api.Middleware;
 using Imagine.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions()
+{
+    Args = args,
+    ApplicationName = typeof(Program).Assembly.FullName
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -13,7 +17,7 @@ builder.Services.AddDbContext<ArtDbContext>(x => x
     .EnableSensitiveDataLogging());
 
 builder.Services.AddAutoMapper(typeof(MappingProfiles));
-
+builder.Services.AddHealthChecks();
 builder.Services.AddApplicationServices();
 builder.Services.AddSwaggerDocumentation();
 builder.Services.AddCors(opt => opt.AddDefaultPolicy(x => x
@@ -31,15 +35,35 @@ var services = scope.ServiceProvider;
 
 if (app.Environment.IsDevelopment())
 {
+    // app.UseDeveloperExceptionPage();
     await ArtDbContextSeed.SeedAsync(services);
 }
 else
 {
     await using var context = services.GetRequiredService<ArtDbContext>();
     await context.Database.MigrateAsync();
+    // app.UseExceptionHandler("/Error");
+    
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // app.UseHsts();
 }
 
+// # If the app calls UseStaticFiles, place UseStaticFiles before UseRouting.
+app.UseStaticFiles();
+
+// If the app uses CORS scenarios, such as [EnableCors], place the call to UseCors before any other middleware that use CORS (for example, place UseCors before UseAuthentication, UseAuthorization, and UseEndpoints).
+app.UseCors();
+
+// Calling UseAuthentication and UseAuthorization adds the authentication and authorization middleware. These middleware are placed between UseRouting and UseEndpoints so that they can:
+//
+// See which endpoint was selected by UseRouting.
+//     Apply an authorization policy before UseEndpoints dispatches to the endpoint.
+app.UseAuthorization();
+// app.UseAuthentication();
+
 app.UseSwaggerDocumentation();
+
+app.MapHealthChecks("/healhtz");
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
--- src/Imagine.Core/Entities/Art.cs
+++ src/Imagine.Core/Entities/Art.cs
@@ -7,7 +7,7 @@ public class Art : BaseEntity
     public string Title { get; set; }
     public string Url { get; set; }
     public int Progress { get; set; }
-    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.Now;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
     public User User { get; set; }
     public int UserId { get; set; }
     public ArtSetting ArtSetting { get; set; }
