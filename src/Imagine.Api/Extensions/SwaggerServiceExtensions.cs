f1f5ba3652daacc0c0c348b089272d34ebfbf3aa
Vitaliy Zema <vitaliy.zema@gmail.com>
2024-01-30 15:02:24 +0300

    Add swagger Authorize button

--- src/Imagine.Api/Extensions/SwaggerServiceExtensions.cs
+++ src/Imagine.Api/Extensions/SwaggerServiceExtensions.cs
@@ -7,20 +7,43 @@ public static class SwaggerServiceExtensions
     public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
     {
         services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen(o => o.SwaggerDoc("v1", new OpenApiInfo()
+        services.AddSwaggerGen(o =>
         {
-            Title = "API",
-            Version = "v1"
-        }));
+            o.SwaggerDoc("v1", new OpenApiInfo()
+            {
+                Title = "Imagine API",
+                Version = "v1"
+            });
+
+            var securitySchema = new OpenApiSecurityScheme
+            {
+                Description = "JWT Auth Bearer Scheme",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            };
+            
+            o.AddSecurityDefinition("Bearer", securitySchema);
+            var securityRequirement = new OpenApiSecurityRequirement
+            {
+                {securitySchema, new[] {"Bearer"}}
+            };
+            o.AddSecurityRequirement(securityRequirement);
+        });
         return services;
     }
 
     public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
     {
-        
         app.UseSwagger();
         app.UseSwaggerUI(o => o.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-        
+
         return app;
     }
 }
