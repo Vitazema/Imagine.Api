67b94bbc1c11ce587c34b32c74bd5d1bf4ac43c9
Vitaliy Zema <vitaliy.zema@gmail.com>
2024-02-19 18:43:52 +0300

    Add ElasticSearch logging

---
+++ src/Imagine.Api/Extensions/LoggingConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Sinks.Elasticsearch;
+
+namespace Imagine.Api.Extensions;
+
+public static class LoggingConfiguration
+{
+    public static void Configure(IConfiguration configuration)
+    {
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .Enrich.WithExceptionDetails()
+            .Enrich.WithMachineName()
+            .WriteTo.Debug()
+            .WriteTo.Console()
+            .WriteTo.Elasticsearch(ConfigureElasticSearch(configuration, environment))
+            .Enrich.WithProperty("Environment", environment)
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+    }
+
+    static ElasticsearchSinkOptions ConfigureElasticSearch(IConfiguration configuration, string environment)
+    {
+        var elasticUri = configuration["ElasticConfiguration:Uri"];
+        var elasticIndex =
+            $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}";
+
+        if (elasticUri != null)
+            return new ElasticsearchSinkOptions(new Uri(elasticUri))
+            {
+                AutoRegisterTemplate = true,
+                IndexFormat = elasticIndex
+            };
+
+        throw new InvalidOperationException("ElasticSearch Uri is not found");
+    }
+}
--- src/Imagine.Api/Program.cs
+++ src/Imagine.Api/Program.cs
@@ -6,6 +6,7 @@ using Imagine.Core.Configurations;
 using Imagine.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.FileProviders;
+using Serilog;
 using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(new WebApplicationOptions()
@@ -14,56 +15,62 @@ var builder = WebApplication.CreateBuilder(new WebApplicationOptions()
     ApplicationName = typeof(Program).Assembly.FullName
 });
 
-builder.Services.Configure<AppSettings>(builder.Configuration.GetSection(nameof(AppSettings)));
-builder.Services.Configure<WorkersSettings>(builder.Configuration.GetSection(nameof(WorkersSettings)));
-builder.Services.AddControllers(
-    // Todo: cause duplication controller calls
-    // c => c.Filters.Add<PermissionsCheckServiceFilter>()
-);
-builder.Services.AddDbContext<ArtDbContext>(x => x
-        .UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
-        .EnableSensitiveDataLogging()
-    // todo: cause AspNetUser unique constrains issue // .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
-);
-builder.Services.AddSingleton<IConnectionMultiplexer>(c =>
-{
-    var configuration = ConfigurationOptions.Parse(
-        builder.Configuration.GetConnectionString("RedisConnection") ?? string.Empty,
-        true);
-    return ConnectionMultiplexer.Connect(configuration);
-});
+LoggingConfiguration.Configure(builder.Configuration);
+builder.Host.UseSerilog();
 
-builder.Services.AddIdentityServices(builder.Configuration);
-builder.Services.AddApplicationServices(builder.Configuration);
+try
+{
+    builder.Services.Configure<AppSettings>(builder.Configuration.GetSection(nameof(AppSettings)));
+    builder.Services.Configure<WorkersSettings>(builder.Configuration.GetSection(nameof(WorkersSettings)));
+    builder.Services.AddControllers(
+        // Todo: cause duplication controller calls
+        // c => c.Filters.Add<PermissionsCheckServiceFilter>()
+    );
+    builder.Services.AddDbContext<ArtDbContext>(x => x
+            .UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+            .EnableSensitiveDataLogging()
+        // todo: cause AspNetUser unique constrains issue // .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+    );
+    builder.Services.AddSingleton<IConnectionMultiplexer>(c =>
+    {
+        var configuration = ConfigurationOptions.Parse(
+            builder.Configuration.GetConnectionString("RedisConnection") ?? string.Empty,
+            true);
+        return ConnectionMultiplexer.Connect(configuration);
+    });
+
+    builder.Services.AddIdentityServices(builder.Configuration);
+    builder.Services.AddApplicationServices(builder.Configuration);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddSwaggerDocumentation();
+    builder.Services.AddSwaggerDocumentation();
 
-builder.Services.AddCustomAutoMapper();
-builder.Services.AddHealthChecks()
-    .AddNpgSql(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException());
+    builder.Services.AddCustomAutoMapper();
+    builder.Services.AddHealthChecks()
+        .AddNpgSql(builder.Configuration.GetConnectionString("DefaultConnection") ??
+                   throw new InvalidOperationException());
 
-builder.Services.AddCors(opt => opt.AddDefaultPolicy(x => x
-    .AllowAnyMethod()
-    .AllowAnyHeader()
-    .AllowAnyOrigin()
-));
+    builder.Services.AddCors(opt => opt.AddDefaultPolicy(x => x
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowAnyOrigin()
+    ));
 
 // if (builder.Environment.IsDevelopment())
 // {
 //     builder.Services.AddDirectoryBrowser();
 // }
 
-var app = builder.Build();
-AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+    var app = builder.Build();
+    AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
-app.DbInitialize();
+    app.DbInitialize();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwaggerDocumentation();
-    // app.UseDeveloperExceptionPage();
-}
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwaggerDocumentation();
+        // app.UseDeveloperExceptionPage();
+    }
 // else
 // {
 // app.UseExceptionHandler("/Error");
@@ -74,42 +81,51 @@ if (app.Environment.IsDevelopment())
 
 
 // # If the app calls UseStaticFiles, place UseStaticFiles before UseRouting.
-var storageDir = app.Configuration.GetSection(nameof(AppSettings)).Get<AppSettings>().StorageDir;
+    var storageDir = app.Configuration.GetSection(nameof(AppSettings)).Get<AppSettings>().StorageDir;
 
-if (storageDir == null) throw new Exception("Cannot get storage directory");
-if (!Path.Exists(storageDir))
-{
-    Directory.CreateDirectory(storageDir);
-}
+    if (storageDir == null) throw new Exception("Cannot get storage directory");
+    if (!Path.Exists(storageDir))
+    {
+        Directory.CreateDirectory(storageDir);
+    }
 
-app.UseFileServer(new FileServerOptions
-{
-    FileProvider = new PhysicalFileProvider(storageDir),
-    RequestPath = "/storage",
-    EnableDirectoryBrowsing = true
-});
+    app.UseFileServer(new FileServerOptions
+    {
+        FileProvider = new PhysicalFileProvider(storageDir),
+        RequestPath = "/storage",
+        EnableDirectoryBrowsing = true
+    });
 
 // If the app uses CORS scenarios, such as [EnableCors], place the call to UseCors before any other middleware that use CORS (for example, place UseCors before UseAuthentication, UseAuthorization, and UseEndpoints).
-app.UseCors();
+    app.UseCors();
 
 // Calling UseAuthentication and UseAuthorization adds the authentication and authorization
 // middleware. These middleware are placed between UseRouting and UseEndpoints so that they can:
 // See which endpoint was selected by UseRouting.
 //     Apply an authorization policy before UseEndpoints dispatches to the endpoint.
-app.UseAuthentication();
-app.UseAuthorization();
+    app.UseAuthentication();
+    app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
-app.UseMiddleware<ExceptionMiddleware>();
+    app.UseMiddleware<ExceptionMiddleware>();
 
 // Add endpoints for error handling. Redirects to errors controller.
 // app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
 
-app.MapHealthChecks("/healthz", new HealthCheckOptions()
-{
-    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-});
-app.MapControllers();
+    app.MapHealthChecks("/healthz", new HealthCheckOptions()
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+    app.MapControllers();
 
-app.Run();
+    app.Run();
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
